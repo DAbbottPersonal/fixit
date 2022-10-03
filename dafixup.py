ed31f3872b7d5c1f9248fc42ae328e47800c624d
Dale Abbott <DaleAbbott999@gmail.com>
2022-10-02 23:59:13 -0400

    ADD: Common cmd function for repeat code

--- dafixup.py
+++ dafixup.py
@@ -12,24 +12,36 @@ logging.basicConfig(format=FORMAT)
 logger = logging.getLogger("dafixup")
 
 
+def _run_cmd(
+    pkg_to_run: str,
+    pkg_desc: str = "",
+    path: Union[str, Path] = ".",
+) -> None:
+    """
+    Command to run a package.
+    """
+
+    path = path.absolute() if isinstance(path, Path) else Path(path).absolute()
+    cmd = [
+        pkg_to_run,
+        str(path),
+    ]
+    cmd_str = " ".join(cmd)
+    logger.warning(f"Start %s: %s", pkg_desc, cmd_str)
+    subprocess.call(cmd, shell=True)
+    logger.warning(f"Finish %s", pkg_desc)
+
+
 def run_import_sort(sort_path: Union[str, Path] = ".") -> None:
     """
     Run the import sorter.
     """
 
-    sort_path = (
-        sort_path.absolute()
-        if isinstance(sort_path, Path)
-        else Path(sort_path).absolute()
-    )
-    cmd = [
+    _run_cmd(
         "isort",
-        str(sort_path),
-    ]
-    cmd_str = " ".join(cmd)
-    logger.warning(f"Start Import Sorter: %s", cmd_str)
-    subprocess.call(cmd, shell=True)
-    logger.warning(f"Finish Import Sorter: %s", cmd_str)
+        "Import Sorter",
+        sort_path,
+    )
 
 
 def run_lint(lint_path: Union[str, Path] = ".") -> None:
@@ -37,19 +49,11 @@ def run_lint(lint_path: Union[str, Path] = ".") -> None:
     Run the linter.
     """
 
-    lint_path = (
-        lint_path.absolute()
-        if isinstance(lint_path, Path)
-        else Path(lint_path).absolute()
-    )
-    cmd = [
+    _run_cmd(
         "black",
-        str(lint_path),
-    ]
-    cmd_str = " ".join(cmd)
-    logger.warning(f"Start Linter: %s", cmd_str)
-    subprocess.call(cmd, shell=True)
-    logger.warning(f"Finish Linter: %s", cmd_str)
+        "Linter",
+        lint_path,
+    )
 
 
 def run_all() -> None:
